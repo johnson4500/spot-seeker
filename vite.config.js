77325d8090e06cb834c324e20ab733ce74a075c9
johnson4500 <johnsonhan6500@gmail.com>
2023-11-24 01:29:23 -0500

    commit github pages change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/skateSpots/"
+  base: "/skateSpots/",
+  root: Path.join(__dirname, 'src')
 })
