635eaa1cd8501154604aaf7cf4d6507d24edc9d2
johnson4500 <johnsonhan6500@gmail.com>
2024-01-12 21:20:15 -0500

    Implemented comment section

--- src/components/SpotInfoContainer.jsx
+++ src/components/SpotInfoContainer.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { React, useMemo, useState, useEffect} from "react";
 import { Slide } from "react-slideshow-image";
+import {update, ref as dbRef, onValue} from 'firebase/database';
+import { rtDB} from '../firebaseconfig';
+import { v4 } from "uuid";
 export function SpotInfoContainer({
   properties,
   spotID,
@@ -7,8 +10,82 @@ export function SpotInfoContainer({
   spotClick,
   showMap
 }) {
+
+  const [text, setText] = useState("");
+  const [commentsData, setCommentsData] = useState({});
+  const commentRef = useMemo(() => dbRef(rtDB, 'spots/' + filteredData[spotID].spotName + '/comments'), [filteredData, spotID]);
+
+
+  const handleChange = (e) => {
+    const textarea = e.target;
+    textarea.style.height = "25px";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+    setText(e.target.value);
+  };
+
+  const postComment = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const day = currentDate.getDate();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+
+    const formattedDateTime = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day} ${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    if (text.length > 1) {
+      update(commentRef, {
+        [v4()]: text + formattedDateTime,
+      }).then(() => {
+        setText("");
+        console.log("Comment added.");
+      });
+    } 
+  }
+  
+  useEffect(() => {
+    const unsubscribe = onValue(commentRef, (snapshot) => {
+      const newComments = [];
+      snapshot.forEach(childSnapshot => {
+        newComments.push(childSnapshot.val());
+      });
+
+      // Sort comments by date posted (oldest to newest)
+      const sortedComments = newComments.slice().sort((a, b) => {
+        const timestampA = a.slice(a.length - 16, a.length); // Extract timestamp substring
+        const timestampB = b.slice(b.length - 16, b.length);
+      
+        const timeA = new Date(timestampA);
+        const timeB = new Date(timestampB);
+      
+        return timeA - timeB;
+      });
+      
+      setCommentsData(sortedComments);
+    });
+
+    return () => unsubscribe(); // Cleanup function to unsubscribe from the event listener
+  }, [commentRef]);
+  
+
   return (
   <>
+  {/* <div 
+    style = {{
+      boxSizing: "border-box",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      borderTopStyle: "solid",
+      borderLeftStyle: "solid", 
+      borderRightStyle: "solid", 
+      borderRadius: "20px",
+      top: "90%", 
+      position: "absolute",
+      height: "73px", 
+      width: "20%",
+      backgroundColor: "white"
+    }}> */}
+  {/* </div> */}
   <div className="spotInfoContainer">
     <div className="imageContainer">
       <div className='spotImage'>
@@ -20,19 +97,75 @@ export function SpotInfoContainer({
           </Slide>
         </div>
       </div>
-      <br></br>
-      <strong id="spotTitleText">Spot Name: {filteredData[spotID].spotName}</strong>
+      <div style = {{marginTop: "10px"}}>
+        <strong id="spotTitleText">Spot Name: {filteredData[spotID].spotName}</strong>
+      </div>
       <p id="spotAddress">Address: {filteredData[spotID].spotAddress}</p>
       <div className="spotDescription">
         <p id="spotDescriptionText">{filteredData[spotID].spotDescription}</p>
       </div>
-      <br></br>
       <button id="backButton" onClick={() => {
         spotClick();
         showMap();
       }}> Back</button>
-  </div>
-  </>
+      <div> 
+        <strong style = {{fontSize: "25px"}}>
+          Comments
+        </strong>
+      </div>
+      {/* <div className = "commentContainer" style = {{height: "500px", width: "90%", backgroundColor: "gray", borderRadius: "20px", borderStyle: "solid", borderWidth: "5px"}}> */}
+  
+      <div className="textAreaContainer">
+        <textarea
+          className="textArea"
+          placeholder="Enter your comment here..."
+          name="comment"
+          value={text}
+          onChange={handleChange}
+        >
+        </textarea>
+        <div 
+          style = {{
+            height: "40px",
+            width: "40px",
+            marginBottom: "10px", 
+            float: "right",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }} 
+          className= "sendIcon"
+          role="button"
+          tabIndex="0"
+          onClick={postComment}
+        >
+          <img id = "sendImg" src = "https://cdn.iconscout.com/icon/free/png-256/free-send-forward-arrow-right-direction-import-30559.png" 
+          style = {{
+            height: "20px", 
+            width: "20px", 
+            left: "10px",
+            color: "blue"
+          }}
+          />
+        </div>
+      </div>
+  
+      <div className = "commentsContainer">
+        {commentsData.length > 0 ? (
+          commentsData.map(element => (
+          <div className="comments">
+            <div style = {{height: "30px"}}>
+            <p style = {{marginBottom: "0"}}>{element.substring(element.length - 16, element.length)}</p>
+            </div>
+             <strong>{element.substring(0, element.length - 16)}</strong>
+          </div>
+          ))
+        ) : (
+          null
+        )}
+      </div>
+    </div>
+    </>
   )
 }
   
